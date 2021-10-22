d693dd13a5f3b293cce6ebbee96ee4cf7e817592
nacho-vilvas <jvilchisc1800@alumno.ipn.mx>
2021-10-22 17:13:47 -0500

    parte del JS

---
+++ chat.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+console.log(_dirname);
+
+app.listen(3000, function(){
+    console.log('el servidor ha iniciado');
+})
---
+++ main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    var message = $('#chat-message');
+    var chat =$('#chat');
+
+    message.focus();
+
+    $('#message-box').submit(function(e){
+        e.preventDefault();
+        chat.append(message.val() + "<br/>");
+    });
+});
